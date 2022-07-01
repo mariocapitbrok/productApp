16ab47dfc5f7f8d47199f9429ab025d189c24510
Mario Capitbrok <mario@capitbrok.com>
2022-06-30 19:33:43 -0500

    http and express working together.

---
+++ .history/backend/app_20220630192942.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello express!</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  ;`Server is running on port: ${PORT}`
+})
--- .history/backend/app_20220630193251.js
+++ .history/backend/app_20220630193251.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello express 2!</h1>')
+  response.send('<h1>Hello express!</h1>')
 })
 
 module.exports = app
