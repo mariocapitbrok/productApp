9b0602a1797d0d2e6daeb3d97e296a64628e0f43
Mario Capitbrok <mario@capitbrok.com>
2022-07-01 12:57:35 -0500

    Add a configuration utility.

---
+++ .history/backend/app_20220701123126.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const productsRouter = require('./controllers/products')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+app.use('/api/products', productsRouter)
+
+module.exports = app
---
+++ .history/backend/simpleTest_20220701124859.js
@@ -0,0 +1,3 @@
+const config = require('./utils/config')
+
+console.log(config.PORT, config.MONGODB_URI)
