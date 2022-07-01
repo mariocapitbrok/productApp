986a6fdcad48464040b2cb057b155ba0265e9767
Mario Capitbrok <mario@capitbrok.com>
2022-06-30 19:04:22 -0500

    Backend, express server test.

--- .history/backend/index_20220630190129.js
+++ .history/backend/index_20220630190129.js
@@ -2,11 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello express!</h1>')
-})
-
 const PORT = 3001
-app.listen(PORT, () => {
+app.connect(PORT, () => {
   ;`Server is running on port: ${PORT}`
 })
--- .history/backend/index_20220630190235.js
+++ .history/backend/index_20220630190235.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello express!</h1>')
+  response.send('<h1>Hello express</h1>')
 })
 
 const PORT = 3001
-app.listen(PORT, () => {
+app.connect(PORT, () => {
   ;`Server is running on port: ${PORT}`
 })
