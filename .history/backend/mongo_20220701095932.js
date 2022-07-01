076652460cd0ee3612bd6c02f3108fda3d224a72
Mario Capitbrok <mario@capitbrok.com>
2022-07-01 10:49:37 -0500

    Mongoose ready.

---
+++ .history/backend/mongo_20220701095932.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://trackstar:${password}@cluster0.18ikmbc.mongodb.net/?retryWrites=true&w=majority`
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Boolean,
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+mongoose
+  .connect(url)
+  .then(result => {
+    console.log('connected')
+
+    const product = new Product({
+      name: 'Wireless Controller Keyboard for PS5',
+      description:
+        'Bluetooth 3.0 Mini Portable Gamepad Chatpad with Built-in Speaker & 3.5MM Audio Jack for Playstation 5 Voice Chat Board for Messaging and Gaming Live Chat',
+      price: 26.99,
+    })
+
+    return product.save()
+  })
+  .then(() => {
+    console.log('product saved!')
+    return mongoose.connection.close()
+  })
+  .catch(err => console.log(err))
